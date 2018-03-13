0ee8d613a0b4f26e6d2ec0e3cafba59fe20aa47e
andreferreira4 <andreferreia4@gmail.com>
2018-03-13 15:10:44 +0000

    Conclusão da primeira ficha para entregar

---
+++ ficha 1 entregar/Ex4.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Escreva a primeira nota: "))
+nota2 = float(input("Escreva a segunda nota: "))
+nota3 = float(input("Escreva a terceira nota: "))
+
+media =float((nota1 * 25 + nota2 * 35 + nota3 * 40) /100)
+media_inteira = int((nota1 * 25 + nota2 * 35 + nota3 * 40) //100)
+media_modulo = int((nota1 * 25 + nota2 * 35 + nota3 * 40)%100)
+print("A média das notas é: {}\n A média inteira é: {}\n O resto da média é {}".format(media,media_inteira,media_modulo))
