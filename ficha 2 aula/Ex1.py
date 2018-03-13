1a97f8924314b1f4c4c108db3c3e53284c68ef04
andreferreira4 <andreferreia4@gmail.com>
2018-03-13 16:15:52 +0000

    ex1

---
+++ ficha 2 aula/Ex1.py
@@ -0,0 +1,11 @@
+#Exercício 1
+
+numero1 = input("Ler primeiro número ")
+numero2 = input("Ler segundo número ")
+
+if numero1 == numero2:
+    print(numero1)
+elif numero1 > numero2:
+    print(numero1)
+else:
+    print(numero2)
