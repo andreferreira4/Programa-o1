3afff1b2e6c69d61c7178dd666b8b3705bad068e
andreferreira4 <andreferreia4@gmail.com>
2018-05-04 11:46:48 +0100

    jogo do galo

--- Trabalho Prático 1/Jogo do Galo.py
+++ Trabalho Prático 1/Jogo do Galo.py
@@ -4,28 +4,25 @@ class jogador():
     def __init__(self):
         self.nome = ""
         self.token = ""
-
 class tabuleiro():
 
-    def __init__(self,):
-        self.tabuleiro = [[None, None ,None ], 
+    def __init__(self):
+        self.tabuleiro = [[ "X" , None ,None ], 
                           [None, None ,None ], 
                           [None, None ,None ]]
-
+        self.token = ""
+    
     def printTabuleiro(self):
         print(' | A | B | C |')
+        print( '--------------------------')
+        print( '1|  {}    |  {}    | {} |'.format(self.tabuleiro[0][0], self.tabuleiro[0][1], self.tabuleiro[0][2]))
         print( '--------------')
-        print( '1| '   '  | '   '  | '   '  |')
+        print( '2|  {}    | {}     | {}  |') 
         print( '--------------')
-        print( '2| '   '  | '   '  | '   '  |') 
-        print( '--------------')
-        print( '3| '   '  | '   '  | '   '  |')
-        print( '--------------')
+        print( '3| {}     | {}     | {}  |')
+        print( '--------------------------')
 
-tabuleiro1 = tabuleiro()
-tabuleiro1.tabuleiro = [ [None, None ,None ], 
-                         [None, None ,None ], 
-                         [None, None ,None ] ]
+
     
 jogador1 = jogador()
 jogador1.nome = input("Nome do jogador: ")
@@ -40,7 +37,7 @@ while True:
         jogador2.token = input("Token que o jogador 2 que prente usar: ")
     else:
         break
-
+tabuleiro1 = tabuleiro()
 tabuleiro1.printTabuleiro()
 print(jogador1.nome,jogador1.token,jogador2.nome,jogador2.token)
 
@@ -48,7 +45,9 @@ jogar = tabuleiro()
 numero_max_jogadas = 9
 while numero_max_jogadas <= 9:
     jogada = input("Escreva a posição que pretende jogar (A1-C3)")
-    
+    jogador1
+
+
 
 
 
