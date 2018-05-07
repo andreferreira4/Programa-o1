b35f6c482d5868ce59ec23f6bd4d862bf345ac8a
andreferreira4 <andreferreia4@gmail.com>
2018-05-07 15:08:15 +0100

    Jogo do Galo

--- Trabalho Prático 1/Jogo do Galo.py
+++ Trabalho Prático 1/Jogo do Galo.py
@@ -2,8 +2,10 @@
 class jogador():
     
     def __init__(self, nome, token):
+        
         self.nome = nome
         self.token = token
+
 class tabuleiro():
 
     def __init__(self):
@@ -14,6 +16,7 @@ class tabuleiro():
     
     def __str__(self):
         return ' |  A   |   B  |   C  |\n-----------------------------\n1|   {}   |   {}   |   {}   |\n-----------------------------\n2|   {}   |   {}   |   {}   |\n-----------------------------\n3|   {}   |   {}   |   {}   |\n-----------------------------'.format(self.tabuleiro[0][0], self.tabuleiro[0][1], self.tabuleiro[0][2],self.tabuleiro[1][0], self.tabuleiro[1][1], self.tabuleiro[1][2],self.tabuleiro[2][0], self.tabuleiro[2][1], self.tabuleiro[2][2])
+    
     def validar_jogada(self,jogada,token):
         if (jogada[0] == "A" or jogada[0] == "B" or jogada[0] == "C") and (jogada[1] == "1" or jogada[1] == "2" or jogada[1] == "3"):
             if jogada[0] == "A":
@@ -43,7 +46,19 @@ class tabuleiro():
         else:
             jogada = input("Escreva a posição que pretende jogar (A1-C3)")
             self.validar_jogada(jogada,token)
-
+    
+    def vencedor(self,nome,token):
+        if  (self.tabuleiro[0][0] == token and self.tabuleiro[0][1] == token and self.tabuleiro[0][2] == token) or \
+            (self.tabuleiro[1][0] == token and self.tabuleiro[1][1] == token and self.tabuleiro[1][2] == token) or \
+            (self.tabuleiro[2][0] == token and self.tabuleiro[2][1] == token and self.tabuleiro[2][2] == token) or \
+            (self.tabuleiro[0][0] == token and self.tabuleiro[1][0] == token and self.tabuleiro[2][0] == token) or \
+            (self.tabuleiro[1][0] == token and self.tabuleiro[1][1] == token and self.tabuleiro[1][2] == token) or \
+            (self.tabuleiro[2][0] == token and self.tabuleiro[2][1] == token and self.tabuleiro[2][2] == token) or \
+            (self.tabuleiro[0][0] == token and self.tabuleiro[1][1] == token and self.tabuleiro[2][2] == token) or \
+            (self.tabuleiro[2][0] == token and self.tabuleiro[1][1] == token and self.tabuleiro[0][2] == token):
+            global vencedor
+            vencedor = True
+            return vencedor
 jogador1nome = input("Nome do jogador: ")
 jogador1token = input("Token que o jogador1 pretende usar: ")
 jogador1 = jogador(jogador1nome, jogador1token)
@@ -63,14 +78,24 @@ jogar.__str__()
 print(jogador1.nome,jogador1.token,jogador2.nome,jogador2.token)
 
 jogar = tabuleiro()
-numero_max_jogadas = 0
+numero_max_jogadas = 1
+vencedor = False
 while numero_max_jogadas <= 9:
     jogada = input("Escreva a posição que pretende jogar (A1-C3)")
     jogar.validar_jogada(jogada,jogador1.token)
     print(jogar)
+    jogar.vencedor(jogador1.nome,jogador1.token)
+    if vencedor == True:
+        print("O vencedor é o: ",jogador1.nome)
+        break
     jogada = input("Escreva a posição que pretende jogar (A1-C3)")
     jogar.validar_jogada(jogada,jogador2.token)
     print(jogar)
+    jogar.vencedor(jogador2.nome,jogador2.token)
+    if vencedor == True:
+        print("O vencedor é o: ",jogador2.nome)
+        break
     numero_max_jogadas += 1
 
-
+if numero_max_jogadas == 9:
+    print("empate")
