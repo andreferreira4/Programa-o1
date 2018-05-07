fa36ef4ce91d3e8b49e8f5015566bfdac116a4af
andreferreira4 <andreferreia4@gmail.com>
2018-05-07 18:28:16 +0100

    jogo do galo

--- Trabalho Prático 1/Jogo do Galo.py
+++ Trabalho Prático 1/Jogo do Galo.py
@@ -12,11 +12,10 @@ class tabuleiro():
         self.tabuleiro = [["", "" , "" ], 
                           ["", "" ,"" ], 
                           ["", "" ,"" ]]
-        self.token = ""
     
     def __str__(self):
         return ' |  A   |   B  |   C  |\n-----------------------------\n1|   {}   |   {}   |   {}   |\n-----------------------------\n2|   {}   |   {}   |   {}   |\n-----------------------------\n3|   {}   |   {}   |   {}   |\n-----------------------------'.format(self.tabuleiro[0][0], self.tabuleiro[0][1], self.tabuleiro[0][2],self.tabuleiro[1][0], self.tabuleiro[1][1], self.tabuleiro[1][2],self.tabuleiro[2][0], self.tabuleiro[2][1], self.tabuleiro[2][2])
-    
+    #Função que nos permite verificar a jogada inserida pelo o jogador e inserir a mesma na lista
     def validar_jogada(self,jogada,token):
         if (jogada[0] == "A" or jogada[0] == "B" or jogada[0] == "C") and (jogada[1] == "1" or jogada[1] == "2" or jogada[1] == "3"):
             if jogada[0] == "A":
@@ -46,7 +45,7 @@ class tabuleiro():
         else:
             jogada = input("Escreva a posição que pretende jogar (A1-C3)")
             self.validar_jogada(jogada,token)
-    
+    #Função que permite verificar se existe vencedor do jogo
     def vencedor(self,nome,token):
         if  (self.tabuleiro[0][0] == token and self.tabuleiro[0][1] == token and self.tabuleiro[0][2] == token) or \
             (self.tabuleiro[1][0] == token and self.tabuleiro[1][1] == token and self.tabuleiro[1][2] == token) or \
@@ -59,13 +58,17 @@ class tabuleiro():
             global vencedor
             vencedor = True
             return vencedor
+
+#Inserir dados do jogador1
 jogador1nome = input("Nome do jogador: ")
 jogador1token = input("Token que o jogador1 pretende usar: ")
 jogador1 = jogador(jogador1nome, jogador1token)
 
+#Inserir dados do jogador2
 jogador2nome = input("Nome do jogador: ")
 jogador2token = input("Token que o jogador2 pretende usar: ")
 
+#Verificar e modificar se o token do jogador2 é igual ao jogador1
 while True:
     if jogador1token == jogador2token: 
         jogador2token = input("Token que o jogador 2 que prente usar: ")
@@ -75,13 +78,14 @@ while True:
 
 jogar = tabuleiro()
 print(jogar)
-print(jogador1.nome,jogador1.token,jogador2.nome,jogador2.token)
+print("Jogador1 {}: {} jogador2 {}: {}".format(jogador1.nome,jogador1.token,jogador2.nome,jogador2.token))
 
 jogar = tabuleiro()
 numero_max_jogadas = 0
 vencedor = False
+# Este ciclo serve para enquanto a 9 jogadas não forem jogadas ou não existir vencedor o programa continua a pedir jogadas aos utilizadores
 while numero_max_jogadas < 9 :
-    jogada = input("Escreva a posição que pretende jogar (A1-C3)")
+    jogada = input("{}, escreva a posição que pretende jogar (A1-C3): ".format(jogador1.nome))
     if jogada == "Desistir" or jogada == "desistir":
         break
     jogar.validar_jogada(jogada,jogador1.token)
@@ -93,7 +97,7 @@ while numero_max_jogadas < 9 :
     numero_max_jogadas += 1
     if numero_max_jogadas == 9:
         break
-    jogada = input("Escreva a posição que pretende jogar (A1-C3)")
+    jogada = input("{}, escreva a posição que pretende jogar (A1-C3): ".format(jogador2.nome))
     if jogada == "Desistir" or jogada == "desistir":
         break
     jogar.validar_jogada(jogada,jogador2.token)
@@ -104,5 +108,6 @@ while numero_max_jogadas < 9 :
         break
     numero_max_jogadas += 1
 
+#Se as 9 jogadas forem feitas e não exister vencedor temos empate
 if numero_max_jogadas == 9:
     print("empate")
